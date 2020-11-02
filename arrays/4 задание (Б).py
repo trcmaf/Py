c63e7ed02cfc48bc775f0d4d02e5d2a234f05e34
trcmaf <anna.v.sokolova20@gmail.com>
2020-11-02 09:16:41 +0300

    init

--- arrays/4 задание (Б).py
+++ arrays/4 задание (Б).py
@@ -1,9 +1,9 @@
 a = []
-j = int(input("Введите количество элементов >> "))
-for i in range(j):
+N = int(input("Введите количество элементов >> "))
+for i in range(N):
     a.append(int(input("Введите элемент массива >> ")))
 temp=0
-for i in range (j-1):
+for i in range (N-1):
     temp = a[i]
     a[i] = a[i+1]
     a[i+1] = temp
