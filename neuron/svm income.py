f695cd9b4bd8041e528a60f15fb912dd4e486765
trcmaf <anna.v.sokolova20@gmail.com>
2022-05-06 22:02:59 +0300

    init

--- neuron/svm income.py
+++ neuron/svm income.py
@@ -16,16 +16,12 @@ with open(input_file, 'r') as f:
     for line in f.readlines():
         if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
             break
-
         if '?' in line:
             continue
-
         data = line[:-1].split(', ')
-
         if data[-1] == '<=50K' and count_class1 < max_datapoints:
             X.append(data)
             count_class1 += 1
-
         if data[-1] == '>50K' and count_class2 < max_datapoints:
             X.append(data)
             count_class2 += 1
@@ -50,7 +46,7 @@ classifier = OneVsOneClassifier(LinearSVC(random_state = 0))
 
 classifier.fit(X, Y)
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2, random_state=5)
-classifier = OneVsOneClassifier(LinearSVC(random_state = 0))#-----------
+classifier = OneVsOneClassifier(LinearSVC(random_state = 0))
 classifier.fit(X_train, Y_train)
 y_test_pred = classifier.predict(X_test)
 
@@ -65,7 +61,7 @@ for i, item in enumerate(input_data):
     if item.isdigit():
         input_data_encoded[i] = int(input_data[i])
     else:
-        input_data_encoded[i] = int(label_encoder[count].transform([input_data[i]]))
+        input_data_encoded[i] = int(label_encoder[count].transform([input_data[i]])) # input_data_encoded[i] = int(label_encoder[count].transform([input_data[i]]))
         count += 1
 input_data_encoded = np.array([input_data_encoded])
 
